8e829df2b7b55c6b4a956763fbf46b0070dfd010
James Floretta <jfloretta001@student.butte.edu>
2017-01-31 22:23:41 +0000

    Lab1.2 Commit

---
+++ lab12/Lab1.2.cpp
@@ -0,0 +1,15 @@
+//Name: James Floretta
+//ASCII Art: Dancing Kirby party
+ 
+#include <iostream>
+using namespace std;
+
+int main()  {
+
+    cout<<"♪♪"<<endl;
+    cout<<"(>'-')>"<<endl;
+    cout<<"<('-'<)"<<endl;
+    cout<<"^('-')^"<<endl;
+    cout<<"<(^_^)>"<<endl;
+}
+ 
