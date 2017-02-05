e6ceb07efffbdc3599ec8e063078370aa2277d53
James Floretta <jfloretta001@student.butte.edu>
2017-02-05 01:25:43 +0000

    Lab1.4 submission

---
+++ lab14/Lab1.4.cpp
@@ -0,0 +1,41 @@
+//James Floretta
+//Coinstar machine
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cents;
+    double total;
+    
+    
+    cout<<"Enter total amount of coins (whole number): "; //Enter any whole number
+    cin>>total;
+    cents = total;
+    
+    
+    cout<<"You entered " << cents / 25 << " quarters";
+    cents = cents % 25;
+    
+    cout<<", " << cents / 10 << " dimes";
+    cents = cents % 10;
+    
+    cout<<", " << cents / 5 << " nickels";
+    cents = cents % 5;
+    
+    cout<<", " << cents / 1 <<" pennies.";
+    cents = cents % 1;
+    
+   cout<<" That is " << "$" <<total / 100 << "."<<endl; //Converting to dollar amount
+   
+   cout<<"After the fee, you take home " << "$" << (total - int(0.109 * total)) / 100<< "."; //What you're left with after the fee
+   
+   
+   
+   
+   
+   
+   
+    
+    
+}
