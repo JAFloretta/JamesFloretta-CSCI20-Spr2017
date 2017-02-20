71258704a13454971a84e3b381a4ef4367398b71
James Floretta <jfloretta001@student.butte.edu>
2017-02-20 21:13:19 +0000

    1.0 Assignment submission

--- Assignment1/1.0Assignment.cpp
+++ Assignment1/1.0Assignment.cpp
@@ -1,5 +1,5 @@
 //James Floretta
-//Assignment 1, Program #1
+//Calculating gross pay and net pay after health insurance deduction and other taxes.
 //Date: 2/16/2017
 
 #include<string>
@@ -12,63 +12,24 @@ struct Workers {
     int hoursWorkedThisWeek;
 };
 
-int healthTax = 130;
-double otherTaxes = 0.17;
-double grossPay;
-double grossPay1;
-double grossPay2;
-double grossPay3;
-
 int main() {
-    //First worker
-    Workers JohnDoe;
-    JohnDoe.name = "John Doe";
-    JohnDoe.hourlyWage = 8.50;
-    cout<<"Enter hours worked this week for John Doe: ";
-    cin>>JohnDoe.hoursWorkedThisWeek;
-    cout<<"- John Doe's weekly earnings - "<< endl;
-    cout<<"Hourly rate: $8.50" << endl;
-    cout<<"Hours worked this week: " << JohnDoe.hoursWorkedThisWeek << endl;
-    double grossPay = (JohnDoe.hourlyWage * JohnDoe.hoursWorkedThisWeek); //gross pay
+    
+    int healthTax = 130;
+    double otherTaxes = 0.17;
+    
+    Workers Employee;
+    cout<<"Enter the name of the employee: ";
+    cin>>Employee.name;
+    cout<<"Enter the employees' hourly wage: ";
+    cin>>Employee.hourlyWage;
+    cout<<"Enter hours worked this week: ";
+    cin>>Employee.hoursWorkedThisWeek;
+    cout<<"- " <<Employee.name<< "'s weekly earnings - "<< endl; //Header information
+    cout<<"Hourly rate: " << Employee.hourlyWage << endl;
+    cout<<"Hours worked this week: " << Employee.hoursWorkedThisWeek << endl;
+    double grossPay = (Employee.hourlyWage * Employee.hoursWorkedThisWeek); //gross pay = hourly wage * hours worked
     cout<<"Gross pay: $" << grossPay <<endl; 
-    cout<<"Net pay: $" << (grossPay - healthTax) - (grossPay * otherTaxes)<<endl; // net pay
+    cout<<"Net pay: $" << (grossPay - healthTax) - (grossPay * otherTaxes)<<endl; //net pay = (gross pay - healthtax) - (gross pay * other taxes)
     
-    //Second worker
-    Workers ShaleseRay;
-    ShaleseRay.name = "Shalese Ray";
-    ShaleseRay.hourlyWage = 15.00;
-    cout<<"Enter the amount of hours Shalese Ray worked this week: ";
-    cin>>ShaleseRay.hoursWorkedThisWeek;
-    cout<<"- Shalese Ray's weekly earnings - "<< endl;
-    cout<<"Hourly rate: $15.00" << endl;
-    cout<<"Hours worked this week: " << ShaleseRay.hoursWorkedThisWeek << endl;
-    double grossPay1 = (ShaleseRay.hourlyWage * ShaleseRay.hoursWorkedThisWeek); //gross pay
-    cout<<"Gross pay: $" << grossPay1 <<endl; 
-    cout<<"Net pay: $" << (grossPay1 - healthTax) - (grossPay1 * otherTaxes)<<endl; // net pay
-    
-    //Third worker
-    Workers AliviaHope;
-    AliviaHope.name = "Alivia Hope";
-    AliviaHope.hourlyWage = 25.15;
-    cout<<"Enter the amount of hours Alivia Hope worked this week: ";
-    cin>>AliviaHope.hoursWorkedThisWeek;
-    cout<<"- Alivia Hope's weekly earnings - "<< endl;
-    cout<<"Hourly rate: $25.15" << endl;
-    cout<<"Hours worked this week: " << AliviaHope.hoursWorkedThisWeek << endl;
-    double grossPay2 = (AliviaHope.hourlyWage * AliviaHope.hoursWorkedThisWeek); //gross pay
-    cout<<"Gross pay: $" << grossPay2 << endl; 
-    cout<<"Net pay: $" << (grossPay2 - healthTax) - (grossPay2 * otherTaxes)<<endl; // net pay
-    
-    //Fourth worker
-    Workers JazzyKerber;
-    JazzyKerber.name = "Jazzy Kerber";
-    JazzyKerber.hourlyWage = 10.00;
-    cout<<"Enter the amount of hours Jazzy Kerber worked this week: ";
-    cin>>JazzyKerber.hoursWorkedThisWeek;
-    cout<<"- Jazzy Kerber's weekly earnings - "<< endl;
-    cout<<"Hourly rate: $10.00" << endl;
-    cout<<"Hours worked this week: " << JazzyKerber.hoursWorkedThisWeek << endl;
-    double grossPay3 = (JazzyKerber.hourlyWage * JazzyKerber.hoursWorkedThisWeek); //gross pay
-    cout<<"Gross pay: $" << grossPay3 <<endl; 
-    cout<<"Net pay: $" << (grossPay3 - healthTax) - (grossPay3 * otherTaxes); // net pay
+   
 }
