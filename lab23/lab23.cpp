16c9efdb88f5f52bcd5434def8c66c490c77fe06
James Floretta <jfloretta001@student.butte.edu>
2017-02-28 22:59:54 +0000

    lab23 submission

---
+++ lab23/lab23.cpp
@@ -0,0 +1,50 @@
+/*  Name: James Floretta
+    Date: 2/28/2017
+    About: A random number generator that accepts user input. User decides
+    which of the numbers are the minimum and maximum and also swaps values.
+*/
+   
+#include <string>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int minimum;
+int maximum;
+int firstSwapValue;
+int secondSwapValue;
+
+//Swap function
+void swapNum (int minimum, int maximum, int& firstSwapValue, int& secondSwapValue) {
+    
+    firstSwapValue = maximum;
+    secondSwapValue = minimum;
+    
+}
+
+
+int randomNumber;
+//Random number generator
+void randomNum(int minimum, int maximum, int& firstSwapValue, int& secondSwapValue) {
+    swapNum (minimum, maximum, firstSwapValue, secondSwapValue);
+    srand(time(0));
+    randomNumber = rand() % firstSwapValue + secondSwapValue;
+    
+    cout<<"Your random value is: " << randomNumber;
+    
+}
+
+
+int main() {
+   
+   cout<<"Enter your minimum value: "; // user input
+   cin>>minimum;
+   
+   cout<<"Enter your maximum value: ";
+   cin>>maximum;
+   
+   randomNum(minimum, maximum, firstSwapValue, secondSwapValue); // function call
+
+    
+}
+
