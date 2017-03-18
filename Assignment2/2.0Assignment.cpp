55f87c35af6936cf8bd828f4b199e7bf57239072
James Floretta <jfloretta001@student.butte.edu>
2017-03-18 20:46:54 +0000

    2.0Assignment submission

--- Assignment2/2.0Assignment.cpp
+++ Assignment2/2.0Assignment.cpp
@@ -1,14 +1,15 @@
 /*  James Floretta
-    Date: 3/10/2017
-    About: Using a distance class to convert between miles, meters, inches, feet or yards.
+    Date: 3/18/2017
+    About: User enters an acceptable value and that value will be converted using a distance converting class. 
+           Uses accessors and mutators that convert between miles, meters, inches, yards and feet.
 */
 
 #include <string>
 #include <iostream>
 using namespace std;
 
-
-class DistanceConverter {
+//Class name 
+class DistanceConverter { 
     private:
         double meter = 0; 
         double inchInMeter = 0.0254;
@@ -16,13 +17,15 @@ class DistanceConverter {
         double yardInMeter = 0.9144;
         double mileInMeter = 1609.344;
     public:
-        void SetMeter(double value) {
+        //Overload constructor
+        void SetMeter(double value) { 
             meter = value;
         }
         double GetMeter() {
             return meter;
         }
-
+        
+        //Gets and sets all values between different units of measurement using function setters and variable getters
         void SetInches(double value) {
             meter = value * inchInMeter;
         }
@@ -50,27 +53,66 @@ class DistanceConverter {
         double GetMiles() {
             return meter / mileInMeter;
         }
-
         
-        
-        
-        void printDistances() {
+        //Print functions
+        void printMiles() {
             cout << "Miles to meter: " << GetMeter() << endl;
             cout << "Miles to inches: " << GetInches() << endl;
             cout << "Miles to feet: " << GetFeet() << endl;
             cout << "Miles to yards: " << GetYards() << endl;
             cout << "Miles to miles: " << GetMiles() << endl;
         }
+        
+        void printFeet() {
+            cout << "Feet to meter: " << GetMeter() << endl;
+            cout << "Feet to inches: " << GetInches() << endl;
+            cout << "Feet to feet: " << GetFeet() << endl;
+            cout << "Feet to yards: " << GetYards() << endl;
+            cout << "Feet to miles: " << GetMiles() << endl;
+        }
     
-    
+        void printInches() {
+            cout << "Inches to meter: " << GetMeter() << endl;
+            cout << "Inches to inches: " << GetInches() << endl;
+            cout << "Inches to feet: " << GetFeet() << endl;
+            cout << "Inches to yards: " << GetYards() << endl;
+            cout << "Inches to miles: " << GetMiles() << endl;
+        }
+
+        void printYards() {
+            cout << "Yards to meter: " << GetMeter() << endl;
+            cout << "Yards to inches: " << GetInches() << endl;
+            cout << "Yards to feet: " << GetFeet() << endl;
+            cout << "Yards to yards: " << GetYards() << endl;
+            cout << "Yards to miles: " << GetMiles() << endl;
+        }
+        void printMeters() {
+            cout << "Meters to meter: " << GetMeter() << endl;
+            cout << "Meters to inches: " << GetInches() << endl;
+            cout << "Meters to feet: " << GetFeet() << endl;
+            cout << "Meters to yards: " << GetYards() << endl;
+            cout << "Meters to miles: " << GetMiles() << endl;
+        }
 };
 
 
 int main() {
+    //Object creation
     DistanceConverter distanceConverter;
-
-    distanceConverter.SetMiles(5);
-    distanceConverter.printDistances();
-
+   
+    //Sets values and prints
+    distanceConverter.SetMiles(1);
+    distanceConverter.printMiles();
     
+    distanceConverter.SetFeet(16);
+    distanceConverter.printFeet();
+    
+    distanceConverter.SetInches(100);
+    distanceConverter.printInches();
+
+    distanceConverter.SetYards(20);
+    distanceConverter.printYards();
+    
+    distanceConverter.SetMeter(1);
+    distanceConverter.printMeters();
 }
