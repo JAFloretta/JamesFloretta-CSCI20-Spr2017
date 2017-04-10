b01f06145bad0ab681b6d664e7ef716362660190
James Floretta <jfloretta001@student.butte.edu>
2017-04-10 19:44:18 +0000

    3.0Assignment submission

--- Assignment3/3.0Assignment.cpp
+++ Assignment3/3.0Assignment.cpp
@@ -4,67 +4,75 @@
  */
 
 
-#include <stdbool.h>
+
 #include <string>
 #include <iostream>
 using namespace std;
 
-int ROUNDS;
-int USER_CHOICE;
-int SCORE;
-int PLAY;
+int rounds = 0;
+int value = 0;
+int user_choice = 0;
+
 
 class ComputersChoice { //Class that contains the random number function for the AI player
     private:
-        int randNum = rand() % 5 + 1;
+        int randNum;
         
     public:
         
-        void SetNum(int value) { //Program runs slow for some reason without this parameter and setting it equal to randNum
-           randNum = value;
+        ComputersChoice() {
+            randNum = value();
+        }
+        int GetChoice() {
+            return randNum;
+        }
+        int value() {
+            randNum = rand() % 5 + 1;
+            return randNum;
         }
         
-        int GetNum() {
-            return randNum;
-        }  
         
         void printResult() {
         if (randNum == 1) {
-            cout <<"The computer chose rock.";
+            cout <<"The computer chose rock. ";
         }
         
         else if (randNum == 2) {
-            cout<<"The computer chose paper.";
+            cout<<"The computer chose paper. ";
         }
         
         else if (randNum == 3) {
-            cout<<"The computer chose scissors.";
+            cout<<"The computer chose scissors. ";
         }
         
         else if (randNum == 4) {
-            cout<<"The computer chose lizard.";
+            cout<<"The computer chose lizard. ";
         }    
         
         else {
-            cout<<"The computer chose spock.";
+            cout<<"The computer chose spock. ";
         }    
             
             
         }       
 };
 
+
+
+
+
 //How many rounds does the user want to play function
 int GetRounds() {  
     
     bool isValid = false;
-    int ROUNDS = 0;
+    
     
     while (!isValid) {
     
     cout<<"Do you want to play 3, 5 or 7 rounds?" << endl;
-    cin>>ROUNDS;
+    cin>>rounds;
     
-    switch(ROUNDS) {
+    switch(rounds) {
         case 3:
         case 5: 
         case 7:
@@ -73,84 +81,108 @@ int GetRounds() {
         default:
             cout<<"Invalid input. Enter 3, 5 or 7."<<endl;
             break;
+
 }
- }
-       
-    cout<<"You picked the best of " << ROUNDS << " rounds." << endl;
-        return ROUNDS;
+  
+}
+           
+    cout<<"You picked the best of " << rounds << " rounds." << endl;
+        return rounds;
+
 }    
 
-
-int PlayRound() {
-    cout<<"Enter 1 for rock, 2 for paper, 3 for scissors, 4 for lizard or 5 for spock."<<endl;
-    cin>>USER_CHOICE;
+//User input
+int GetUser() {
     
     
     
-    for (int i = 0; i < ROUNDS; i++) {
-       
-       
-        
-        
-    }
+    bool isValid = false;
     
     
+    while (!isValid) {
     
-}
-
-
+    cout<<"Enter 1 for rock, 2 for paper, 3 for scissors, 4 for lizard, 5 for spock." << endl;
+    cin>>user_choice;
     
-
-
-
-int main() {
-     GetRounds();
-     
-   
-   
-   
-    
-
-    //User Input
-    
-    
-    switch(USER_CHOICE) {
+    switch(user_choice) {
         case 1:
-            cout<<"You chose rock." << endl;;
+            isValid = true;
+            cout<<"You chose rock. ";
             break;
-        case 2:
-            cout<<"You chose paper." << endl;
+        case 2: 
+            isValid = true;
+            cout<<"You chose paper. ";
             break;
         case 3:
-            cout<<"You chose scissors." << endl;
+            isValid = true;
+            cout<<"You chose scissors. ";
             break;
+           break;
         case 4:
-            cout<<"You chose lizard." << endl;
+            isValid = true;
+            cout<<"You chose lizard. ";
             break;
         case 5:
-            cout<<"You chose spock." << endl;
+            isValid = true;
+            cout<<"You chose spock. ";
             break;
         default:
-            cout<<"Invalid input. Enter 1 for rock, 2 for paper, 3 for scissors, 4 for lizard or 5 for spock." << endl;
+            cout<<"Invalid input."<<endl;
             break;
-        
+
         
     }
+}
+}
+ 
+int GetScore() {
+    int compScore = 0;
+    int humanScore = 0;
+    int tie = 0;
     
+    if (user_choice == value) {
+        tie++;
+    }
+    
+    else if (user_choice == 1 && value == 2 || value == 5) {
+        humanScore++;
+     }
+    
+    else if (user_choice == 2 && value == 1 || value == 5) {
+        humanScore++;
+    }
+    
+    else if (user_choice == 3 && value == 2 || value == 4) {
+        humanScore++;
+    }
+    
+    else if (user_choice == 4 && value == 5 || value == 2) {
+        humanScore++;
+    }
+    
+    else if (user_choice == 5 && value == 3 || value == 1) {
+        humanScore++;
+    }
+    
+    else {
+        compScore++;
+    }
+        cout<<"Human score: " << humanScore << " Computer score: " << compScore << " Ties: " << tie << endl;
+} 
+ 
+int main() {
+    GetRounds();
+    
+    for (int i = 1; i <= rounds; i++) {
+    ComputersChoice A;
     srand(time(NULL));
+    GetUser();
+    A.printResult();
     
-    //Object creation
-    ComputersChoice randomNumber;
+    cout<<"This was round " << i << "." << " On to round " << i + 1 << "." << endl;
     
-    //Fetches the random number and prints results accordingly
-    randomNumber.GetNum();
-    randomNumber.printResult();
-    
-    
-
-    
-    
-    
-    
+   
+}
+   GetScore();
     
 }
