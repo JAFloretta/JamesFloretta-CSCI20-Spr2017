a61e2ab6bd8a087f009dd32e92e24036ddcab5f5
James Floretta <jfloretta001@student.butte.edu>
2017-04-17 22:28:57 +0000

    lab41 submission

---
+++ lab41/lab41.cpp
@@ -0,0 +1,70 @@
+/* James Floretta
+    4/17/2017
+    A single array that is able to find the minimum and maximum element as well as find even, odd, total sum, search and sort elements.
+*/    
+    
+
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+
+class ArrayTools {
+    private:
+       int arr[], int s;
+       
+    public:
+        void SetFind_sum(int firstNumber, int secondNumber) {
+            a = firstNumber;
+            b = secondNumber;
+        }
+        
+        int GetFind_sum() {
+            return a + b;
+        }
+      
+        
+        
+};
+
+void Arraytools..(int a[], int s) {
+    for..
+    arr[i] = a[i];
+    
+}
+
+
+
+
+
+
+int main() {
+
+    const int SIZE = 10;
+    int myArray[SIZE];
+    
+
+    
+    for(int i = 0; i < SIZE; i++){
+        cin>>myArray[i];
+    }
+    ArrayTools a(myArray); // sends myArray through the class
+    
+    //a.Print();
+    //cout<<"Min: "<<a.Find_min(0, 4)<<endl;
+    //cout<<"Max: "<<a.Find_max(5, 10)<<endl;
+    cout<<"Sum = "<<a.Find_sum()<<endl;
+   // cout<<"Search 10"<<a.Search(10)<<endl;
+   // cout<<"Sorted? "<<a.Is_sorted()<<endl;
+    
+    return 0;
+
+}
+  
+   
+   
+   
+   
+    
