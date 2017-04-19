4aa15cdd2f46e6a0dd334a9cf5c9481da6b07b5f
James Floretta <jfloretta001@student.butte.edu>
2017-04-19 22:56:50 +0000

    lab43 submission

--- lab43/lab43.cpp
+++ lab43/lab43.cpp
@@ -16,8 +16,8 @@ using namespace std;
 class Item {
     private:
         vector<string> Name; // Vector to initialize array in the constructor
-        double Price;
-        int Quantity;
+        vector<double> Price;
+        vector<int> Quantity;
         
         
     public:
@@ -30,16 +30,16 @@ class Item {
         
         
         string GetName(int index);
-        double GetPrice();
-        int GetQuantity();
+        double GetPrice(int p_index);
+        int GetQuantity(int q_index);
         
        
 };
 
 Item::Item() { // Constructor. Utilizing vector to initialize array
-    Name = {"bow $15 - ","arrow $2 - ","axe $30 - ","sword $25 - ","mace $35 - ","flail $20 - ","scepter $45 - ","wand $55 - ","armor $60 - ","staff $50"};
-    Price = 0.0;
-    Quantity = 0;
+    Name = {"bow $15","arrow $2","axe $30","sword $25","mace $35","flail $20","scepter $45","wand $55","armor $60","staff $50"};
+    Price = {15,2,30,25,35,20,45,55,60,50};
+    Quantity = {1,1,1,1,1,1,1,1,1,1};
 }
 
 Item::Item(string itemName, double itemPrice, int itemQuantity) { // Overload constructor
@@ -54,23 +54,23 @@ void Item::SetName(string itemName) {
 }
 
 void Item::SetPrice(double itemPrice) {
-    Price = itemPrice;
+    Price.push_back(itemPrice);
 }
 
 void Item::SetQuantity(int itemQuantity) {
-    Quantity = itemQuantity;
+    Quantity.push_back(itemQuantity);
 }
 
 string Item::GetName(int index) {
     return Name[index];
 } 
 
-double Item::GetPrice() {
-    return Price;
+double Item::GetPrice(int p_index) {
+    return Price[p_index];
 }
 
-int Item::GetQuantity() {
-    return Quantity;
+int Item::GetQuantity(int q_index) {
+    return Quantity[q_index];
 }
 
 
@@ -79,12 +79,35 @@ int Item::GetQuantity() {
 
 
 int main() {
-     Item ShowItems;
-
+    Item ShowItems;
+    Item ShowPrice;
+    Item ShowQuantity;
+    
+    string userChoice;
+    int totalPrice;
+    int addItem = 0;
+    
 	 cout<<"Welcome to the battle store. Pick what you need for your quest." << endl;
 	 for (int index = 0; index < 10; index++) {
-	     cout<<ShowItems.GetName(index);
+	     cout<<ShowItems.GetName(index) << endl;
 	 }
+      cin>>userChoice;    
+    
+     if (userChoice == "bow") {
+         
+         for (int p_index = 0; p_index < 1; p_index++) {
+             cout<<"You chose a bow. That costs $" << ShowPrice.GetPrice(p_index);
+         }
+        
+            for (int q_index = 0; q_index < 1; q_index++) {
+                
+                cout<<" There are " << ShowQuantity.GetQuantity(q_index)<< " bows left in stock";
+            }
+         
+         
+         
+     }
+
 
 	    
 
